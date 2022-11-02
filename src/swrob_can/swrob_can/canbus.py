3590c7aba7d090a5532be8d9dfaf329e94722c41
PC2721 <pengcheng201921@gmail.com>
2022-11-02 22:26:55 +0800

    update new interfaces and can receive function

--- src/swrob_can/swrob_can/canbus.py
+++ src/swrob_can/swrob_can/canbus.py
@@ -1,28 +1,15 @@
 from dataclasses import dataclass
+from socket import MsgFlag
 from sqlite3 import DatabaseError
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
 from can_interfaces.msg import Can
+from motordata_interfaces.msg import Motor
 from .CAN_Recv import*
 from .CAN_Send import*
 from .Monitor import Motor_Monitor
-
-
-'''
-class MotorControlCan:
-    def __init__(
-        self,
-        fun_num,
-        motor_id,
-        data
-    ):
-        self.num = fun_num
-        self.id = motor_id
-        self.data = data
-
-MotorControl = MotorControlCan()
-'''
+from .CAN_Recv import Motor1
 
 
 class CanBus(Node):
@@ -34,39 +21,32 @@ class CanBus(Node):
     def __init__(self,name):
         super().__init__(name)
         self.get_logger().info("test node:%s" % name)
-        #self.pub_data = self.create_publisher(String,"motor",10)
-        #self.time_period = 0.001
-       # self.timer = self.create_timer(self.time_period,self.timer_callback)
-
+        self.pub_data = self.create_publisher(Motor,"motor",10)
+        self.time_period = 0.001
+        self.timer = self.create_timer(self.time_period,self.timer_callback)
         self.recv_data = self.create_subscription(Can,"motorcontrol",self.recvdata_callback,10)
 
-    #def timer_callback(self):
-      #  self.pub_data(Motor1)
+    def timer_callback(self):
+        msg = Motor()
+        msg.can=Motor1.CAN_Channel
+        msg.motor[0]=Motor1.Motor_Speed
+        msg.motor[1]=Motor1.Current
+        msg.motor[2]=Motor1.Voltage
+        self.pub_data.publish(msg)
+
 
     def recvdata_callback(self,message):
-        if message.data[0]==1:
-            Motor_Stop_Control("can0", message.data[1])
-        if message.data[0]==2:
-            PWM_Control("can0", int(message.data[1]))
-        if message.data[0]==3:
-            Speed_Control("can0", message.data[1])
-        if message.data[0]==4:
-            Torque_Control("can0", message.data[1])
-
-        self.get_logger().info('use fuction%d data%d' %(message.data[0],message.data[1]))
+         if message.data[0]==1:
+             Motor_Stop_Control("can0", message.data[1])
+         if message.data[0]==2:
+             PWM_Control("can0", int(message.data[1]))
+         if message.data[0]==3:
+             Speed_Control("can0", message.data[1])
+         if message.data[0]==4:
+             Torque_Control("can0", message.data[1])
+         self.get_logger().info('use fuction%d data%d' %(message.data[0],message.data[1]))
 
         
-        '''
-        global MotorControl
-        if MotorControl.num == 1:
-            Motor_Stop_Control(MotorControl.id, 1, MotorControl.data)
-        if MotorControl.num == 2:
-            PWM_Contorl(MotorControl.id, 1, MotorControl.data)
-        if MotorControl.num == 3:
-            Speed_Contorl(MotorControl.id, 1, MotorControl.data)
-        if MotorControl.num == 4:
-            Torque_Contorl(MotorControl.id, 1, MotorControl.data)
-        '''
 
 
 
